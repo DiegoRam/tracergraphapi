89b4ad869441b166f4dc34175699978a9131fa9f
diegoram <ddarkheart@gmail.com>
2015-01-06 06:53:22 -0300

    add new test for orientdb

--- test/sandbox/orientdbperformance.scala
+++ test/sandbox/orientdbperformance.scala
@@ -1,6 +1,9 @@
 package sandbox
 
+import java.util.UUID
+
 import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory
+import org.diegoram.User
 import org.specs2.mutable.Specification
 
 
@@ -11,12 +14,36 @@ class OrientDbPerformanceTest extends Specification{
 
   "orient db test" should {
     sequential
-    "create Database" in {
+    "check Database" in {
       db should not beNull
     }
 
-    "create 1000 nodes" in {pending}
-    "create 1M nodes" in {pending}
+    "create a new database" >> {
+      (new OrientGraphFactory("plocal:/Users/diegoram/dev/orientdataTest")).getNoTx should not beNull
+    }
+
+    "create 1000 nodes" >> {
+      val userList = for(i <- 1 to 1000) yield (
+        User(UUID.randomUUID().toString, "Diego_" + i.toString).save
+        )
+      userList.size should beEqualTo(1000)
+    }
+    "create 10K nodes" >> {
+      val userList = for(i <- 1 to 10000) yield (
+        User(UUID.randomUUID().toString, "Diego_" + i.toString).save
+        )
+      userList.size should beEqualTo(10000)
+
+    }
+
+    "create 100K nodes" >> {
+      val userList = for(i <- 1 to 100000) yield (
+        User(UUID.randomUUID().toString, "Diego_" + i.toString).save
+        )
+      userList.size should beEqualTo(100000)
+
+    }
+
     "retrieve a single Node by id" in {pending}
     "retrieve a list of node by a given Node (related ones)" in {pending}
     "retrieve a list of nodes by a given query" in {pending}
